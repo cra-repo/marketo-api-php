b4a3ef62a5779821933971374a34787ec4ef12e0
Konstantin Gusev <guvkon.net@icloud.com>
2021-09-29 19:19:50 +0700

    feat: implement Tags asset endpoint

---
+++ examples/tag.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+use Cra\MarketoApi\Client;
+use Cra\MarketoApi\Endpoint\Asset\Tag;
+use Kint\Kint as K;
+
+$config = json_decode(file_get_contents('../config.json'));
+$client = (new Client($config))->authenticate();
+$tagEndpoint = new Tag($client);
+
+$tags = $tagEndpoint->browse();
+K::dump($tags);
+
+$tags = $tagEndpoint->queryByName('Marketing');
+K::dump($tags);
---
+++ src/Endpoint/Asset/Tag.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cra\MarketoApi\Endpoint\Asset;
+
+use Cra\MarketoApi\Endpoint\EndpointInterface;
+use Cra\MarketoApi\Entity\Asset\Tag as Entity;
+use Exception;
+
+class Tag implements EndpointInterface
+{
+    use AssetTrait;
+
+    /**
+     * Get Tags.
+     *
+     * @param array{maxReturn: ?int, offset: ?int} $optional
+     * @return Entity[]
+     *
+     * @throws Exception
+     */
+    public function browse(array $optional = []): array
+    {
+        $query = [];
+        $this->addFieldsToQuery($query, ['maxReturn', 'offset'], $optional);
+
+        $response = $this->get('/tagTypes.json', ['query' => $query]);
+        $response->checkIsSuccess();
+
+        return $response->isResultValid() ?
+            array_map(static fn(object $tag) => new Entity($tag), $response->result()) :
+            [];
+    }
+
+    /**
+     * Query Tags by name.
+     *
+     * @param string $name
+     * @return Entity|null
+     *
+     * @throws Exception
+     */
+    public function queryByName(string $name): ?Entity
+    {
+        $query = ['name' => $name];
+
+        $response = $this->get('/tagType/byName.json', ['query' => $query]);
+        $response->checkIsSuccess();
+        $result = $response->singleValidResult();
+
+        return $result ? new Entity($result) : null;
+    }
+}
